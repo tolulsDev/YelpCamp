4a8d8510bd6e2c695dac10a891aaf1c6041277cd
tolulsDev <toluls@ymail.com>
2020-04-03 15:27:16 +0100

    initial app commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is YelpCamp! I'll be updating soon
+
+This app will help us add new camps and more..
---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express"),
+    app = express();
+
+app.get("/", (req, res)=> {
+    res.send("Welcome! It's about to go down. New version");
+})
+
+app.listen(3000, ()=>{
+    console.log("Yelp Server's up!");
+});
